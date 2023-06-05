647d051cca79d80b935ff03e310094f9f300199c
dalet <victordalet@protonmail.com>
2023-06-05 22:21:06 +0200

    json metho

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Influences
+
+## Tecno
+![](https://img.shields.io/badge/Node.js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white)
+![](https://img.shields.io/badge/MySQL-005C84?style=for-the-badge&logo=mysql&logoColor=white)
+
+## Installation
+
+#### sql
+````sql
+CREATE TABLE SITE(ID INTEGER PRIMARY KEY, NAME VARCHAR(220),ADDRESS VARCHAR(220),COORDINATES VARCHAR(220));
+````
+
+#### launch
+```shell
+node app.js
+```
--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const mysql = require('mysql');
-const utf8 = require('utf8');
 
 class Main{
     constructor() {
         this.connect_to_db();
         fetch('https://data.iledefrance.fr/api/records/1.0/search/?dataset=principaux-sites-touristiques-en-ile-de-france0&q=&rows=20')
-            .then(r => r.text())
-            .then(r=>this.data = r)
+            .then(r => r.json())
+            .then(r => this.data = r)
             .then(()=>this.separate_data())
     }
-
 
     connect_to_db() {
          this.conn = mysql.createConnection({
@@ -24,22 +22,15 @@ class Main{
         });
     }
 
-    create_table() {
-        this.conn.query('CREATE TABLE SITE(ID INTEGER PRIMARY KEY, NAME VARCHAR(220),ADDRESS VARCHAR(220),COORDINATES VARCHAR(220))')
-    }
-
-
-     separate_data() {
-        this.data = this.data.split('datasetid');
-        for (let i = 1 ; i < this.data.length ; i++) {
-            const name = this.data[i].split('typo_niv3": "')[1].split('",')[0]
-            let adrrese = "";
-            adrrese += this.data[i].split('adresse": "')[1].split('"')[0]
-            adrrese += this.data[i].split('nomcom": "')[1].split('",')[0];
-            const coo = this.data[i].split('coordinates": [')[1].split(']')[0]
-            this.give_data_to_db(i,utf8.decode(name),utf8.decode(adrrese),coo)
+    async separate_data() {
+        const max_iter = this.data['nhits'];
+        for (let i = 0 ; i < max_iter ; i+= 20){
+            for (let j = 0 ; j < 20 ; j++) {
+                this.give_data_to_db(j+i, this.data['records'][j]['fields']['typo_niv3'], this.data['records'][j]['fields']['adresse'], this.data['records'][j]['geometry']['coordinates'][0].toString()+this.data['records'][j]['geometry']['coordinates'][1].toString())
+            }
+            this.data = await fetch('https://data.iledefrance.fr/api/records/1.0/search/?dataset=principaux-sites-touristiques-en-ile-de-france0&q=&rows=20&start='+(i+20).toString())
+            this.data = await this.data.json();
         }
-        this.get_data();
     }
 
 
@@ -48,9 +39,6 @@ class Main{
             [id,name,address,coo]);
     }
 
-    get_data() {
-        this.conn.query("SELECT * FROM SITE",(e,r)=>console.log(r));
-    }
 }
 
 
